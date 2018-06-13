42bb5bec0efe9e1fd0a963dd3db81ce6fc22445e
Nikitosh <npodguzov@yandex.ru>
2018-06-13 17:08:19 +0300

    Fix diploma after review

--- diploma/build.sh
+++ diploma/build.sh
@@ -1,6 +1,6 @@
-xelatex diploma
+xelatex diploma --shell-escape
 bibtex diploma
-xelatex diploma
-xelatex diploma
+xelatex diploma --shell-escape
+xelatex diploma --shell-escape
 rm diploma.aux diploma.bbl diploma.blg diploma.log diploma.out diploma.toc
 chrome diploma.pdf
